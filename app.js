83da2422e3aa40c7e9ff2358072a294d2aef67b7
Keilin99 <mayopb99@gmail.com>
2022-03-21 12:50:25 -0600

    Actualizando el archivo app.js con el feedback solicitado

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const crearOpciones = () => {
                     <label for="tiempo">Seleccione el tipo de clima hoy: </label>
                     <select id="tiempo" onchange="obtenerTiempo()">
                       <option value="">Elija una opción</option>
-                      <option value="soleado">Soleado</option>
-                      <option value="lluvioso">Lluvioso</option>                        <option value="nevando">Nevando</option> 
-                      <option value="nublado">Nublado</option>
+                      <option value="Soleado">Soleado</option>
+                      <option value="Lluvioso">Lluvioso</option>
+                      <option value="Nevando">Nevando</option> 
+                      <option value="Nublado">Nublado</option>
                     </select>
                     <p id="parrafo"></p>
                     <div id="contenedorImagen"></div>
@@ -17,51 +18,31 @@ crearOpciones();
 let elementoSelector = document.querySelector("#tiempo"),
   elementoParrafo = document.querySelector("#parrafo"),
   elementoDiv = document.querySelector("#contenedorImagen");
-const crearImagenSoleado = () => {
-  let imagen = `<img class="imagen" src="img/Soleado.png" alt="Está soleado">`;
-  elementoDiv.innerHTML = imagen;
-};
-const crearImagenLluvioso = () => {
-  let imagen = `<img class="imagen" src="img/Lluvioso.png" alt="Está lluvioso">`;
-  elementoDiv.innerHTML = imagen;
-};
-const crearImagenNevando = () => {
-  let imagen = `<img class="imagen" src="img/Nevando.png" alt="Está nevando">`;
-  elementoDiv.innerHTML = imagen;
-};
-const crearImagenNublado = () => {
-  let imagen = `<img class="imagen" src="img/Nublado.png" alt="Está nublado">`;
+const crearImagen = (imagenes) => {
+  let imagen = `<img class="imagen" src="img/${imagenes}.png" alt="${imagenes}">`;
   elementoDiv.innerHTML = imagen;
 };
 const obtenerTiempo = () => {
   let opcionSeleccionada = elementoSelector.value;
   switch (opcionSeleccionada) {
-    case "soleado":
+    case "Soleado":
       elementoParrafo.textContent =
         "Está soleado afuera hoy, ve a la playa y compra un helado.";
       break;
-    case "lluvioso":
+    case "Lluvioso":
       elementoParrafo.textContent =
         "La lluvia está cayendo afuera, llévate un paraguas.";
       break;
-    case "nevando":
+    case "Nevando":
       elementoParrafo.textContent =
         "La nieve está cayendo, tómate una taza de chocolate caliente.";
       break;
-    case "nublado":
+    case "Nublado":
       elementoParrafo.textContent =
         "Está nublado hoy, podría cambiar en cualquier momento.";
       break;
     default:
       elementoParrafo.textContent = "";
   }
-  opcionSeleccionada === "soleado"
-    ? crearImagenSoleado()
-    : opcionSeleccionada === "lluvioso"
-    ? crearImagenLluvioso()
-    : opcionSeleccionada === "nevando"
-    ? crearImagenNevando()
-    : opcionSeleccionada === "nublado"
-    ? crearImagenNublado()
-    : (elementoDiv.textContent = "");
+  crearImagen(opcionSeleccionada);
 };
